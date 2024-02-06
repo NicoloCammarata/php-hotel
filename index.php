bed35862c1e231023bf45cf35a7c6df960bb2ebc
NicoloCammarata <nicolo.camma@gmail.com>
2024-02-06 16:08:01 +0100

    tabella

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Hotel</title>
 </head>
 <body>
-    <h1>hotel</h1>
+    <h1 class="text-center">hotel</h1>
 <?php
 
     $hotels = [
@@ -50,12 +51,65 @@
     ];
 
 ?>
-    <ul>
-        <?php
-            foreach($hotels as $hotel) {
-                echo '<li>'.$hotel['name'].' - '.$hotel['description'].' | '.$hotel['parking'].'-'.$hotel['vote'].'-'.$hotel['distance_to_center'].'</li>';
-            }
-        ?>
-    </ul>
+  <div class="container p-1">
+    <table class="table table-bordered ">
+        <thead>
+            <tr>
+                <?php
+                    foreach($hotels as $hotel) {
+                        echo '<th>'.$hotel['name'].'</th>' ;
+                    }
+                    
+                ?>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php
+                    foreach($hotels as $hotel) {
+                        echo '<td>'.$hotel['description'].'</td>' ;
+                    }
+                    
+                    
+                ?>
+
+            </tr>
+            <tr>
+                <?php
+                    
+                    foreach($hotels as $hotel) {
+                        echo '<td>'.$hotel['parking'].'</td>' ;
+                    }
+                    
+                ?>
+
+            </tr>
+            <tr>
+                <?php
+                    
+                    foreach($hotels as $hotel) {
+                        echo '<td>'.$hotel['vote'].'</td>' ;
+                    }
+                    
+                ?>
+
+            </tr>
+            <tr>
+                <?php
+                    
+                    foreach($hotels as $hotel) {
+                        echo '<td>'.$hotel['distance_to_center'].'</td>' ;
+                    }
+                    
+                ?>
+
+            </tr>
+        </tbody>
+  
+
+        
+    </table>
+  </div>
 </body>
 </html>
